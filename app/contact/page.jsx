c8e2d796709786b7ac44a9209dc3a0189b1f93ff
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-08-08 12:44:29 +0530

    Updated Contact Page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,132 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { motion } from "framer-motion";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    desc: "+91-7889165948",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "E-Mail",
+    desc: "kabirsharma2607@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    desc: "Jalandhar, Punjab",
+  },
+];
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              action=""
+            >
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                I'm always excited to connect with fellow tech enthusiasts,
+                potential collaborators, and anyone interested in my work. Feel
+                free to reach out to me via email at{" "}
+                <a
+                  href="mailto:kabirsharma2607@gmail.com"
+                  className="text-white"
+                >
+                  kabirsharma2607@gmail.com
+                </a>{" "}
+                or connect with me on{" "}
+                <a
+                  href="https://www.linkedin.com/in/kabirsharma2607"
+                  className="text-white"
+                >
+                  LinkedIn
+                </a>
+                . Alternatively, you can fill out the form below, and I will get
+                back to you as soon as possible. Let's create something amazing
+                together!
+              </p>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="E-Mail adress" />
+                <Input type="phone" placeholder="Phone Number" />
+              </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a Service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a Service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">Android App Development</SelectItem>
+                    <SelectItem value="mst">Data Analytics</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here"
+              />
+              <Button size="md" className="max-w-40">
+                Send Message
+              </Button>
+            </form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+
+                    <h3 className="text-xl">{item.desc}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
